0a35237dedd76e559873abb4d4847c049dbfc044
assasinos <kubasem198@gmail.com>
2024-11-14 15:19:27 +0100

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      "primary": "#9146FF",
+      "background": "#1E1E1E",
+      "accent": "#00D2FF",
+      "secondaryBackground": "#2E0D5E",
+      "textPrimary": "#F1F1F1",
+      "textSecondary": "#A1A1A1",
+      "hover": "#BB86FC",
+      "border": "#3A3A3A"
+    }
   },
   plugins: [],
 }
