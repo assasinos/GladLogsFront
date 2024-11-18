e8cae05e0e758e4ff018d823bd9b71eaf626ecae
assasinos <kubasem198@gmail.com>
2024-11-18 17:39:22 +0100

    Add custom border animation and gradient utility to Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     container: {
       center: true,
@@ -14,17 +12,34 @@ module.exports = {
     },
     extend: {
       colors: {
-        "primary": "#9146FF",
-        "background": "#1E1E1E",
-        "accent": "#00D2FF",
-        "secondaryBackground": "#2E0D5E",
-        "textPrimary": "#F1F1F1",
-        "textSecondary": "#A1A1A1",
-        "hover": "#BB86FC",
-        "border": "#3A3A3A"
-      }
+        primary: "#9146FF",
+        background: "#1E1E1E",
+        accent: "#00D2FF",
+        secondaryBackground: "#2E0D5E",
+        textPrimary: "#F1F1F1",
+        textSecondary: "#A1A1A1",
+        hover: "#BB86FC",
+        border: "#3A3A3A",
+      },
+      animation: {
+        border: "border 4s linear infinite",
+      },
+      keyframes: {
+        border: {
+          to: { "--border-angle": "360deg" },
+        },
+      },
     },
-
   },
-  plugins: [require("tailwindcss-animate")],
-}
+  plugins: [
+    require("tailwindcss-animate"),
+    function ({ addUtilities }) {
+      addUtilities({
+        ".border-gradient": {
+          background:
+            "linear-gradient(45deg, theme(colors.background), theme(colors.background) 50%, theme(colors.background)) padding-box, conic-gradient(from var(--border-angle), theme(colors.slate.600/.48) 80%, theme(colors.indigo.500) 86%, theme(colors.primary) 90%, theme(colors.accent) 94%, theme(colors.slate.600/.48)) border-box",
+        },
+      });
+    },
+  ],
+};
